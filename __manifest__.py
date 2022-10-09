bc6654f5ddda52af117f8afd535a6cf2921ec598
brahim94 <39997129+brahim94@users.noreply.github.com>
2022-10-09 13:30:37 +0100

    Update __manifest__.py
    increase version

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
     'application':'True',
     'images': ['static/description/icon.png'],
 
